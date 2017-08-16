c038dfaf41bce9c670166f06968065b75c74c759
wilsonfisk <wilson.fisk.secure@gmail.com>
2017-08-16 18:54:08 -0400

    Added 'compress.sh' and 'decompress.sh'.

---
+++ compress.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# A tar wrapper script to simplify archiving files.
+
+# help
+if [ $# -eq 0 -o "$1" = -h -o "$1" = --help ] ; then
+    echo "${0##*/} <file1> <*dir>... - archive files, folders..."
+    exit 2
+fi
+
+# file existence test
+for file in "$@" ; do
+    [ ! -e "$file" ] && echo "file non-existent: "$file"" && error=y
+done
+[ "$error" ] && exit 1
+
+# archive name
+read -e -p "archive name [archive.tar.gz]: " arc_nme 
+[ ! "$arc_nme" ] && arc_nme=archive.tar.gz
+
+# program existence test
+shopt -s extglob
+case "$arc_nme" in
+    *.@(tar.bz2|tb@(2|z|z2)) )  p=bzip2      ; hash $p 2>&- \
+                                || { echo program required: $p ; arc_prrq=y ; } ;;
+    *.@(tar.gz|tgz)          )  p=gzip       ; hash $p 2>&- \
+                                || { echo program required: $p ; arc_prrq=y ; } ;;
+    *.tar.lz                 )  p=lzip       ; hash $p 2>&- \
+                                || { echo program required: $p ; arc_prrq=y ; } ;;
+    *.@(tar.lzma|tlz)        )  p=xz         ; hash $p 2>&- \
+                                || { echo program required: $p ; arc_prrq=y ; } ;;
+    *.@(tar.xz|txz)          )  p=xz         ; hash $p 2>&- \
+                                || { echo program required: $p ; arc_prrq=y ; } ;;
+    *.@(tar.Z|t?(a)z)        )  p=gzip       ; hash $p 2>&- \
+                                || { echo program required: $p ; arc_prrq=y ; } ;;
+    *.tar                    )  p=tar        ; hash $p 2>&- \
+                                || { echo program required: $p ; arc_prrq=y ; } ;;
+    *                        )  { echo ext unknown: "$archive" ; arc_prrq=y ; } ;;
+esac ; [ "$arc_prrq" ] && exit 1
+
+# archive overwrite prompt
+[ -f "$arc_nme" ] && while : ; do
+    read -p "archive exists, overwrite? (y/n): " arc_ovr
+    shopt -s extglob
+    if   [[ "$arc_ovr" = [yY]?(e)?(s) ]] ; then
+        opt_ovr=--overwrite
+        break
+    elif [[ "$arc_ovr" = [nN]?(o)     ]] ; then
+        exit 2
+    fi
+done
+
+# archive creation
+tar --create --auto-compress --file "$arc_nme" "$@" $(printf '%s' "$opt_ovr") \
+&& [ -f "$arc_nme" ] && echo "archive created: "$arc_nme""
---
+++ decompress.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+# A wrapper script to decompress various archive types.
+shopt -s extglob
+
+# options: help and list
+if (( ! $# )) \
+|| [[ "$*" =~ (^| )(-h|--help)( |$) ]] || [[ "$*" = @(-l|--list) ]] ; then
+    printf "${0##*/} [*-l] <file>... — wrapper script to decompress various "
+    echo   "archive types"
+    echo   "  -l, --list  - list archive contents"
+    exit 2
+elif [[ "$*" =~ (^| )(-l|--list)( |$) ]] ; then
+    list=y
+fi 
+
+# archive existence test
+for archive in "$@" ; do
+    [[ "$archive" = @(-l|--list) ]] && continue
+    [ ! -f "$archive" ] && { echo archive non-existent: "$archive" ;arc_noex=yes;}
+done ; [ "$arc_noex" ] && exit 1
+
+# program existence test
+prg_test () { hash $prg 2>&- || { echo program required: $prg ; arc_prrq=y ; };}
+
+for archive in "$@" ; do
+case "$archive" in
+    -l | --list              )  continue                   ;;
+    *.tar                    )  prg=tar        ; prg_test  ;;
+    *.@(tar.bz2|tb@(2|z|z2)) )  prg=bzip2      ; prg_test  ;;
+    *.@(tar.gz|tgz)          )  prg=gzip       ; prg_test  ;;
+    *.tar.lz                 )  prg=lzip       ; prg_test  ;;
+    *.@(tar.lzma|tlz)        )  prg=xz         ; prg_test  ;;
+    *.@(tar.xz|txz)          )  prg=xz         ; prg_test  ;;
+    *.@(tar.Z|t?(a)z)        )  prg=gzip       ; prg_test  ;;
+    *.7z                     )  prg=7zr        ; prg_test  ;;
+    *.bz2                    )  prg=bunzip2    ; prg_test  ;;
+    *.exe                    )  prg=cabextract ; prg_test  ;;
+    *.gz                     )  prg=gunzip     ; prg_test  ;;
+    *.lz                     )  prg=lzip       ; prg_test  ;;
+    *.rar                    )  prg=unrar      ; prg_test  ;;
+    *.xz                     )  prg=xz         ; prg_test  ;;
+    *.Z                      )  prg=gzip       ; prg_test  ;;
+    *.zip                    )  prg=unzip      ; prg_test  ;;
+    *                        )  echo extension unknown: "$archive" ; arc_prrq=y ;;
+esac
+done ; [ "$arc_prrq" ] && exit 1
+
+# list
+[ "$list" ] && \
+for archive in "$@" ; do
+    [[ "$archive" = @(-l|--list) ]] && continue
+echo -e "\e[1m "$archive" \e[0m"
+case "$archive" in
+    *.tar                    )  tar tvf           "$archive"      ;;  
+    *.@(tar.bz2|tb@(2|z|z2)) )  tar tvf           "$archive"      ;;  
+    *.@(tar.gz|tgz)          )  tar tvf           "$archive"      ;;  
+    *.tar.lz                 )  tar tvf           "$archive"      ;;  
+    *.@(tar.lzma|tlz)        )  tar tvf           "$archive"      ;;  
+    *.@(tar.xz|txz)          )  tar tvf           "$archive"      ;;  
+    *.@(tar.Z|t?(a)z)        )  tar tvf           "$archive"      ;;  
+    *.7z                     )  7zr l -bd         "$archive"      ;;  
+    *.bz2                    )  echo              "${archive%.*}" ;;  
+    *.exe                    )  cabextract --list "$archive"      ;;  
+    *.gz                     )  echo              "${archive%.*}" ;;  
+    *.lz                     )  echo              "${archive%.*}" ;;  
+    *.rar                    )  unrar l           "$archive"      ;;  
+    *.xz                     )  echo              "${archive%.*}" ;;    
+    *.Z                      )  gzip --list       "$archive"      ;;  
+    *.zip                    )  unzip -l -qq      "$archive"      ;;  
+esac
+done ; [ "$list" ] && exit
+
+# decompress
+arc () { echo "$archive"..." " ; }
+
+for archive in "$@" ; do
+case "$archive" in
+    *.tar                    )  arc ; tar    -xf    "$archive" 2>&1 > /dev/null ;;
+    *.@(tar.bz2|tb@(2|z|z2)) )  arc ; tar    -xf    "$archive" 2>&1 > /dev/null ;;
+    *.@(tar.gz|tgz)          )  arc ; tar    -xf    "$archive" 2>&1 > /dev/null ;;
+    *.tar.lz                 )  arc ; tar    -xf    "$archive" 2>&1 > /dev/null ;;
+    *.@(tar.lzma|tlz)        )  arc ; tar    -xf    "$archive" 2>&1 > /dev/null ;;
+    *.@(tar.xz|txz)          )  arc ; tar    -xf    "$archive" 2>&1 > /dev/null ;;
+    *.@(tar.Z|t?(a)z)        )  arc ; tar    -xf    "$archive" 2>&1 > /dev/null ;;
+    *.7z                     )  arc ; 7zr      x    "$archive" 2>&1 > /dev/null ;;
+    *.bz2                    )  arc ; bunzip2 -k    "$archive" 2>&1 > /dev/null ;;
+    *.exe                    )  arc ; cabextract    "$archive" 2>&1 > /dev/null ;;
+    *.gz                     )  arc ; gunzip  -k    "$archive" 2>&1 > /dev/null ;;
+    *.lz                     )  arc ; lzip    -k -d "$archive" 2>&1 > /dev/null ;;
+    *.rar                    )  arc ; unrar    x    "$archive" 2>&1 > /dev/null ;;
+    *.xz                     )  arc ; unxz    -k    "$archive" 2>&1 > /dev/null ;;
+    *.Z                      )  arc ; gunzip  -k    "$archive" 2>&1 > /dev/null ;;
+    *.zip                    )  arc ; unzip   -q    "$archive" 2>&1 > /dev/null ;;
+esac
+done
